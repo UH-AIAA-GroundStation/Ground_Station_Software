933f432c60bd3b536758e90fee9ba4489ff7c465
Thanh Pham <149126740+BruceTee23@users.noreply.github.com>
2026-06-24 21:16:54 -0500

    GPS test work lol, need to bench mark accuracy

--- scratch_code.py
+++ scratch_code.py
@@ -26,15 +26,25 @@ data_y_2 = []
 longitude_init = -95.390488
 latitude_init = 29.755568
 
+longitude_target = -97.745338
+latitude_target = 30.281140
+
+start = utm.from_latlon(latitude_init,longitude_init)
+
+start_x = start[0]
+start_y = start[1]
+zone = start[2]
+letter = start[3]
+
 longitude = []
 latitude = []
 
-normalize_x = 0
-normalize_y = 0
-normalize_x_init = 0
-normalize_y_init = 0
+# normalize_x = 0
+# normalize_y = 0
+# normalize_x_init = 0
+# normalize_y_init = 0
 
-counter = 0
+# counter = 0
 
 
 class MainWindow(QMainWindow):
@@ -64,6 +74,8 @@ class MainWindow(QMainWindow):
         # self.curve_1.setDownsampling(ds=10, auto=True, method='peak')
         # self.curve_2.setDownsampling(ds=10, auto=True, method='peak')
 
+        
+
         self.gps_curve = self.plot.plot(longitude, latitude, name="GPS", pen="g")
         self.gps_curve.setClipToView(True)
         self.gps_curve.setDownsampling(ds=3, auto=True, method='peak')
@@ -76,35 +88,52 @@ class MainWindow(QMainWindow):
         global data_y_1, data_y_2
         global longitude_init
         global latitude_init
-        global normalize_x, normalize_y
-        global normalize_x_init, normalize_y_init
-        global counter
+        # global normalize_x, normalize_y
+        # global normalize_x_init, normalize_y_init
+        # global counter
+        global start_x
+        global start_y
 
         # Closest constant to get to Austin from Houston
-        longitude_init -= 0.0051
-        latitude_init += 0.00115
+        # longitude_init -= 0.0051
+        # latitude_init += 0.00115
 
-        data_packet = utm.from_latlon(latitude_init,longitude_init)
+        longitude_init += (longitude_target - longitude_init) * 0.01
+        latitude_init += (latitude_target - latitude_init) * 0.01
 
-        if counter == 1: # 1st run (no normalization values yet)
-            normalize_x_init = data_packet[0]
-            normalize_y_init = data_packet[1]
-            normalize_x = data_packet[0]/normalize_x_init
-            normalize_y = data_packet[1]/normalize_y_init
-        elif counter >= 2: # 2nd run (normalization values exist)
-            normalize_x = data_packet[0]/normalize_x_init
-            normalize_y = data_packet[1]/normalize_y_init
+        # Force zone so coordinates don't get reset when zone changes
+        data_packet = utm.from_latlon(latitude_init,longitude_init, force_zone_number=zone)
 
-        counter += 1
+        print(f"Easting: {data_packet[0]}")
+        print(f"Northing: {data_packet[1]}")
 
-        longitude.append(normalize_x)
-        latitude.append(normalize_y)
+        x = data_packet[0] - start_x
+        y = data_packet[1] - start_y
 
-        print(f"Real long: {longitude_init}")
-        print(f"Real lat: {latitude_init}")
+        if longitude_init <= longitude_target and latitude_init >= latitude_target:
+            return
 
-        print(normalize_x)
-        print(normalize_y)
+        # if counter == 1: # 1st run (no normalization values yet)
+        #     normalize_x_init = data_packet[0]
+        #     normalize_y_init = data_packet[1]
+        #     normalize_x = data_packet[0]/normalize_x_init
+        #     normalize_y = data_packet[1]/normalize_y_init
+        # elif counter >= 2: # 2nd run (normalization values exist)
+        #     normalize_x = data_packet[0]/normalize_x_init
+        #     normalize_y = data_packet[1]/normalize_y_init
+
+        # counter += 1
+
+        longitude.append(x)
+        latitude.append(y)
+
+        # print(f"Real long: {longitude_init}")
+        # print(f"Real lat: {latitude_init}")
+
+        # print(normalize_x)
+        # print(normalize_y)
+
+        print(data_packet[2])
 
         self.gps_curve.setData(longitude, latitude)
 
